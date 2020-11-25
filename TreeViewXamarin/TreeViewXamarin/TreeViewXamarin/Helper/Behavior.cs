d1938b2a8f6f237dc064eef1693ce5a85e372764
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-25 15:09:00 +0530

    XAMARIN-34261  - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,68 @@
+﻿using Syncfusion.TreeView.Engine;
+using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin.Helper
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        SfTreeView TreeView;
+        Button AddButton;
+        Button DeleteButton;
+        #endregion
+
+        #region Overrides
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            TreeView = bindable.FindByName<SfTreeView>("treeView");
+            AddButton = bindable.FindByName<Button>("addButton");
+            DeleteButton = bindable.FindByName<Button>("deleteButton");
+
+            AddButton.Clicked += AddButton_Clicked;
+            DeleteButton.Clicked += DeleteButton_Clicked;
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            AddButton.Clicked -= AddButton_Clicked;
+            DeleteButton.Clicked -= DeleteButton_Clicked;
+            AddButton = null;
+            DeleteButton = null;
+            TreeView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            var india = new TreeViewNode() { Content = "India" };
+            var delhi = new TreeViewNode() { Content = "Delhi" };
+            var newDelhi = new TreeViewNode() { Content = "New Delhi" };
+            delhi.ChildNodes.Add(newDelhi);
+            var tamilNadu = new TreeViewNode() { Content = "Tamil Nadu" };
+            var chennai = new TreeViewNode() { Content = "Chennai" };
+            india.ChildNodes.Add(delhi);
+            india.ChildNodes.Add(tamilNadu);
+            tamilNadu.ChildNodes.Add(chennai);
+
+            TreeView.Nodes.Add(india);
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            TreeView.Nodes.RemoveAt(1);
+        }
+
+        #endregion
+    }
+}
